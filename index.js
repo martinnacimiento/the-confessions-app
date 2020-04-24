a9fab88cae180c3631c18f6718707931502781ff
Martin Nacimiento <martinnacimiento98@gmail.com>
2020-04-24 06:39:02 -0300

    a

---
+++ index.js
@@ -0,0 +1,32 @@
+ 
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const helmet = require("helmet");
+const cors = require("cors");
+var port = process.env.PORT || 3000;
+
+
+app.use(helmet());
+app.use(cors())
+
+var confessions = []
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/confessions', (req, res) => {
+    res.json(confessions)
+});
+
+io.on('connection', function(socket){
+    socket.on('add confession', (confession) => {
+        confessions.push(confession);
+        io.emit('add confession', confession);
+    });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
